27fa1a722d137f59b974276cc8da827d7059a57c
Gena <genafom1n@yandex.ru>
2022-05-01 19:43:49 +0300

    - Added "hardness" to Element params - Added temporary method setHardness() - Added template for method calcHardness() - Rethought the concept of optimization... Again... - Started implementing class Generate() where we will have the main stuff: Optimization and final generator - Added simpleTODO not to forget the point I finished. And to have a plan

--- Element/element.py
+++ Element/element.py
@@ -5,7 +5,7 @@ class Element():
     """ Object of type: drone racing track elemenet """
 
     # class constructor
-    def __init__(self, nGates, nFlags, gCord, fCord):
+    def __init__(self, nGates, nFlags, gCord, fCord, hardness = 0):
 
         """ Initializing atributes of our class """
         self.nGates = nGates # number of gates in this element
@@ -26,3 +26,13 @@ class Element():
 
         return Element.grid.getGrid()
 
+    # temporary function, to check if optimization works properly.
+    # After it will be changed with calcHardness(), which formula will be derived later and which will
+    # calculate hardness based on amount of gates and flags + degrees of rotation throughout the element
+    def setHardness(self, hardn):
+        self.hardness = hardn
+
+    # The next gen of setHardness()
+    def calcHardness(self, degrees):
+        return degrees
+    # This function will be futher changes as I am still working on the formula for hardness.
--- generator/generate.py
+++ generator/generate.py
@@ -1,27 +1,20 @@
 from Element.element import Element
 
-# Getting elements we can use for the generation
-# For now we realise algo with usual istream in python, later add telegram bot to handle this stuff and output result
-
-
-def getElems():
-    print("Hi, please enter the nuber of gates and flags you have to build a track (minimum 3 of each is neccessary for"
-          "proper work of algo)")
-    gates = int(input())
-    flags = int(input())
-    print("Please enter the hardness level you wish to have (1 - 10)")
-    hardness = int(input())
-    return gates, flags, hardness
-
-
-# Here we go for introducing hardness metric, which I introduce based on my own experince.
 # We are going to have a base of elements of class "Element" and each of them will have its hardness level, so we will
-# be just optimizing a function of two variables, where x = number of gates, y = number of flags
-# f(x, y) = hardness of track
 
-# Am i A joke to ya? It is a function of N variables, where n will be number of all distinct elements
-# (but not routes, routes will be chosen during generating the track)
+# It is a function f(x, y) = hardness of track.
+# It will look like dis: f(nGates, nFlags) = Summ of elements from 0 to I(HardnessOfElementNormalized^i * number of
+# elements in the track layout)
 
-# def optimizeUsage(nGates, nFlags):
+# It seems like generate will become a class for:
+# a) generating the set of elems
+# b) generating the track
 
+class Generate():
 
+    def __int__(self, elemList = []):
+        self.elemList = elemList
+
+    finalElements = []
+
+    # def optimizeElements(self, targetHardn):
