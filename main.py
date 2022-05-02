4c34380c52f6c1ec4d002bd6b4084ca3c6e6dd4d
Gena <genafom1n@yandex.ru>
2022-05-02 17:43:17 +0300

    - Have an idea for optimizeElements() - Implement it after gaming with friends

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from Element.element import Element
-
-splitS = Element(2, 2, [2, 3], [1, 4])
-x = splitS.fillGrid()
-for i in range(len(x)):
-    stringa = ""
-    for j in range(len(x[i])):
-        stringa += str(x[i][j])
-    print(stringa)
+# from Element.element import Element
+#
+# splitS = Element(2, 2, [2, 3], [1, 4])
+# x = splitS.fillGrid()
+# for i in range(len(x)):
+#     stringa = ""
+#     for j in range(len(x[i])):
+#         stringa += str(x[i][j])
+#     print(stringa)
