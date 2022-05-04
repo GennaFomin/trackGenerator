9fd1172476acdae6786828e46a1ad8fc3f0b8c44
Gena <genafom1n@yandex.ru>
2022-05-04 23:07:39 +0300

    - Some ideas, need to implement

--- generator/generate.py
+++ generator/generate.py
@@ -11,7 +11,11 @@ class Generate():
 
     finalElements = []
 
-    # def optimizeElements(self, targetHardn):
+    def optimizeElements(self, targetHardn):
+        currentHard = 0
+        for elem in self.elemList:
+            # if(currentHard + elem.calc)
+        # need to understand routes before it (
         # Что если просто набивать сложными элементами пока не достигнем поставленной сложности,
         # а потом, будем добивать просто воротами, и просто флагами, сложность которых равна мнимой 1
         # И так получится, что мы сможем использовать максимум элементов + получить нужную сложность )
